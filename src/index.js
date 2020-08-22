99564a5a7edbf57f7deea0036360be3aef5abd56
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-22 15:52:14 +0200

    add dockerode and simple example

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ const enableWs = require('express-ws');
 enableWs(app);
 
 app.listen(8080, () => console.log('Listening on http://127.0.0.1:8080'));
+
+var Docker = require('dockerode');
+var docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+docker.listContainers()
+.then(list => console.log(list))
+.catch(e => console.error(e));
